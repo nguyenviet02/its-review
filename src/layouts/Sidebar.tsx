e53035bec22a08025e2ae6cbeba3c0641c317e5a
Nguyen Viet <nguyenviet02.dev@gmail.com>
2025-02-24 17:14:21 +0700

    chore

--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -1,9 +1,22 @@
-'use client';
+"use client";
 
-import { Dialog, DialogBackdrop, DialogPanel, TransitionChild } from '@headlessui/react';
-import { HomeIcon, XMarkIcon, UserGroupIcon, DocumentTextIcon, CalendarDaysIcon, RectangleGroupIcon, IdentificationIcon } from '@heroicons/react/24/outline';
-import { ROLES } from '@/types';
-import { useMemo } from 'react';
+import {
+  Dialog,
+  DialogBackdrop,
+  DialogPanel,
+  TransitionChild,
+} from "@headlessui/react";
+import {
+  HomeIcon,
+  XMarkIcon,
+  UserGroupIcon,
+  DocumentTextIcon,
+  CalendarDaysIcon,
+  RectangleGroupIcon,
+  IdentificationIcon,
+} from "@heroicons/react/24/outline";
+import { ROLES } from "@/types";
+import { useMemo } from "react";
 
 type SidebarProps = {
   role: ROLES;
@@ -11,41 +24,106 @@ type SidebarProps = {
   setSidebarOpen: (open: boolean) => void;
 };
 
-export default function Sidebar({ role, sidebarOpen, setSidebarOpen }: SidebarProps) {
+export default function Sidebar({
+  role,
+  sidebarOpen,
+  setSidebarOpen,
+}: SidebarProps) {
   const navigation = useMemo(() => {
     if (role === ROLES.SUPER_ADMIN) {
       return [
-        { name: 'Dashboard', href: '/admin', icon: HomeIcon, current: true },
-        { name: 'Quản lý nhân sự', href: '/admin/employees-management', icon: UserGroupIcon, current: false },
-        { name: 'Quản lý đơn từ', href: '/admin/documents-management', icon: DocumentTextIcon, current: false },
-        { name: 'Quản lý kỳ đánh giá', href: '/admin/assessment-period-management', icon: CalendarDaysIcon, current: false },
-        { name: 'Quản lý phòng ban', href: '/admin/departments-management', icon: RectangleGroupIcon, current: false },
-        { name: 'Quản lý Admin', href: '/admin/admin-management', icon: IdentificationIcon, current: false },
+        { name: "Dashboard", href: "/admin", icon: HomeIcon, current: true },
+        {
+          name: "Quản lý nhân sự",
+          href: "/admin/staff-management",
+          icon: UserGroupIcon,
+          current: false,
+        },
+        {
+          name: "Quản lý đơn từ",
+          href: "/admin/documents-management",
+          icon: DocumentTextIcon,
+          current: false,
+        },
+        {
+          name: "Quản lý kỳ đánh giá",
+          href: "/admin/assessment-period-management",
+          icon: CalendarDaysIcon,
+          current: false,
+        },
+        {
+          name: "Quản lý phòng ban",
+          href: "/admin/departments-management",
+          icon: RectangleGroupIcon,
+          current: false,
+        },
+        {
+          name: "Quản lý Admin",
+          href: "/admin/admin-management",
+          icon: IdentificationIcon,
+          current: false,
+        },
       ];
     }
 
     if (role === ROLES.ADMIN) {
       return [
-        { name: 'Dashboard', href: '/admin', icon: HomeIcon, current: true },
-        { name: 'Quản lý nhân sự', href: '/admin/users-management', icon: UserGroupIcon, current: false },
-        { name: 'Quản lý đơn từ', href: '/admin/documents-management', icon: DocumentTextIcon, current: false },
-        { name: 'Quản lý kỳ đánh giá', href: '/admin/assessment-period-management', icon: CalendarDaysIcon, current: false },
-        { name: 'Quản lý phòng ban', href: '/admin/departments-management', icon: RectangleGroupIcon, current: false },
+        { name: "Dashboard", href: "/admin", icon: HomeIcon, current: true },
+        {
+          name: "Quản lý nhân sự",
+          href: "/admin/users-management",
+          icon: UserGroupIcon,
+          current: false,
+        },
+        {
+          name: "Quản lý đơn từ",
+          href: "/admin/documents-management",
+          icon: DocumentTextIcon,
+          current: false,
+        },
+        {
+          name: "Quản lý kỳ đánh giá",
+          href: "/admin/assessment-period-management",
+          icon: CalendarDaysIcon,
+          current: false,
+        },
+        {
+          name: "Quản lý phòng ban",
+          href: "/admin/departments-management",
+          icon: RectangleGroupIcon,
+          current: false,
+        },
       ];
     }
 
-    return [{ name: 'Đơn từ', href: '/', icon: DocumentTextIcon, current: true }];
+    return [
+      { name: "Đơn từ", href: "/", icon: DocumentTextIcon, current: true },
+    ];
   }, [role]);
   return (
     <>
-      <Dialog open={sidebarOpen} onClose={setSidebarOpen} className="relative z-50 lg:hidden">
-        <DialogBackdrop transition className="fixed inset-0 bg-gray-900/80 transition-opacity duration-300 ease-linear data-[closed]:opacity-0" />
+      <Dialog
+        open={sidebarOpen}
+        onClose={setSidebarOpen}
+        className="relative z-50 lg:hidden"
+      >
+        <DialogBackdrop
+          transition
+          className="fixed inset-0 bg-gray-900/80 transition-opacity duration-300 ease-linear data-[closed]:opacity-0"
+        />
 
         <div className="fixed inset-0 flex">
-          <DialogPanel transition className="relative mr-16 flex w-full max-w-xs flex-1 transform transition duration-300 ease-in-out data-[closed]:-translate-x-full">
+          <DialogPanel
+            transition
+            className="relative mr-16 flex w-full max-w-xs flex-1 transform transition duration-300 ease-in-out data-[closed]:-translate-x-full"
+          >
             <TransitionChild>
               <div className="absolute left-full top-0 flex w-16 justify-center pt-5 duration-300 ease-in-out data-[closed]:opacity-0">
-                <button type="button" onClick={() => setSidebarOpen(false)} className="-m-2.5 p-2.5">
+                <button
+                  type="button"
+                  onClick={() => setSidebarOpen(false)}
+                  className="-m-2.5 p-2.5"
+                >
                   <span className="sr-only">Close sidebar</span>
                   <XMarkIcon aria-hidden="true" className="size-6 text-white" />
                 </button>
@@ -63,10 +141,15 @@ export default function Sidebar({ role, sidebarOpen, setSidebarOpen }: SidebarPr
                           <a
                             href={item.href}
                             className={`${
-                              item.current ? 'bg-gray-50 text-indigo-600' : 'text-gray-700 hover:bg-gray-50 hover:text-indigo-600'
+                              item.current
+                                ? "bg-gray-50 text-indigo-600"
+                                : "text-gray-700 hover:bg-gray-50 hover:text-indigo-600"
                             } group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold`}
                           >
-                            <item.icon aria-hidden="true" className={`${item.current ? 'text-indigo-600' : 'text-gray-400 group-hover:text-indigo-600'} size-6 shrink-0`} />
+                            <item.icon
+                              aria-hidden="true"
+                              className={`${item.current ? "text-indigo-600" : "text-gray-400 group-hover:text-indigo-600"} size-6 shrink-0`}
+                            />
                             {item.name}
                           </a>
                         </li>
@@ -84,7 +167,9 @@ export default function Sidebar({ role, sidebarOpen, setSidebarOpen }: SidebarPr
       <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
         {/* Sidebar component, swap this element with another sidebar if you like */}
         <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6 pb-4">
-          <div className="flex h-16 shrink-0 items-center text-3xl font-bold">ITS Review</div>
+          <div className="flex h-16 shrink-0 items-center text-3xl font-bold">
+            ITS Review
+          </div>
           <nav className="flex flex-1 flex-col">
             <ul role="list" className="flex flex-1 flex-col gap-y-7">
               <li>
@@ -93,9 +178,12 @@ export default function Sidebar({ role, sidebarOpen, setSidebarOpen }: SidebarPr
                     <li key={item.name}>
                       <a
                         href={item.href}
-                        className={`${item.current ? 'bg-gray-50 text-indigo-600' : 'text-gray-700 hover:bg-gray-50 hover:text-indigo-600'} group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold`}
+                        className={`${item.current ? "bg-gray-50 text-indigo-600" : "text-gray-700 hover:bg-gray-50 hover:text-indigo-600"} group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold`}
                       >
-                        <item.icon aria-hidden="true" className={`${item.current ? 'text-indigo-600' : 'text-gray-400 group-hover:text-indigo-600'} size-6 shrink-0`} />
+                        <item.icon
+                          aria-hidden="true"
+                          className={`${item.current ? "text-indigo-600" : "text-gray-400 group-hover:text-indigo-600"} size-6 shrink-0`}
+                        />
                         {item.name}
                       </a>
                     </li>
