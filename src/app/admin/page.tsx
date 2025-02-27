161da187964e3b7b904746f10f7066617b4a5742
Nguyen Viet <nguyenviet02.dev@gmail.com>
2025-02-27 15:26:50 +0700

    chore

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -9,18 +9,11 @@ const Admin = async () => {
 
   return (
     <div>
-      {session ? (
         <div>
           <h1>Admin Page</h1>
           <p>Welcome {session?.user?.email}</p>
           <p>{JSON.stringify(session, null, 2)}</p>
         </div>
-      ) : (
-        <div>
-          <h1>Admin Page</h1>
-          <p>Please sign in</p>
-        </div>
-      )}
     </div>
   );
 };
