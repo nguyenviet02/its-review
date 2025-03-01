9b0abc9e941a40b898bc69e6af39dde1dffa5944
Nguyen Viet <nguyenviet02.dev@gmail.com>
2025-03-01 16:19:36 +0700

    chore

--- src/lib/zustand/reviewFormDialogStore.ts
+++ src/lib/zustand/reviewFormDialogStore.ts
@@ -1,14 +1,14 @@
 import { FORM_TYPES } from "@/types";
 import { create } from "zustand";
 
-interface IStaffDialogSummaryInfoStore {
+interface IStaffDialogReviewFormStore {
   isOpen: boolean;
   type: FORM_TYPES;
   openDialog: () => void;
   closeDialog: () => void;
 }
 
-export const useReviewFormDialogStore = create<IStaffDialogSummaryInfoStore>(
+export const useReviewFormDialogStore = create<IStaffDialogReviewFormStore>(
   (set) => ({
     isOpen: false,
     type: FORM_TYPES.GENERAL,
