b865dbbb0481d93a7d5200d5c488d089523770f3
Nguyen Viet <nguyenviet02.dev@gmail.com>
2025-03-04 18:01:40 +0700

    chore

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -91,7 +91,7 @@ export default function Header({ setSidebarOpen }: HeaderProps) {
             </MenuButton>
             <MenuItems
               transition
-              className="absolute right-0 z-10 mt-2.5 w-fit origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+              className="absolute min-w-[150px] right-0 z-10 mt-2.5 w-fit origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
             >
               {userNavigation.map((item) => (
                 <MenuItem key={item.name}>
