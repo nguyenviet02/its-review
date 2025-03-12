c8601e9403383d05dc5e6c16be3981409bddf361
Nguyen Viet <nguyenviet02.dev@gmail.com>
2025-03-12 09:06:52 +0700

    change link tag

--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -184,7 +184,7 @@ export default function Sidebar({
                 <ul role="list" className="-mx-2 space-y-1">
                   {navigation.map((item) => (
                     <li key={item.name}>
-                      <a
+                      <Link
                         href={item.href}
                         className={`${item.current ? "bg-gray-50 text-indigo-600" : "text-gray-700 hover:bg-gray-50 hover:text-indigo-600"} group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold`}
                       >
@@ -193,7 +193,7 @@ export default function Sidebar({
                           className={`${item.current ? "text-indigo-600" : "text-gray-400 group-hover:text-indigo-600"} size-6 shrink-0`}
                         />
                         {item.name}
-                      </a>
+                      </Link>
                     </li>
                   ))}
                 </ul>
