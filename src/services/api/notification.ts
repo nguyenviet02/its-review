de8f448deb266c125dded66a7c0a21781dfaf570
Nguyen Viet <nguyenviet02.dev@gmail.com>
2025-03-21 16:37:11 +0700

    fix: noti

--- src/services/api/notification.ts
+++ src/services/api/notification.ts
@@ -5,14 +5,14 @@ import axiosInstance from "./axiosInstance";
  * Get notifications for the current user
  * @returns Promise with array of notifications
  */
-export const getNotification = async (): Promise<INotificationResponseAPI[]> => {
-  await axiosInstance
-    .get<INotificationResponseAPI[]>(`/api/v1/notifications`)
-    .then(function (response) {
-      return response?.data;
-    })
-    .catch(function (error) {
-      console.log(error.toJSON());
-    });
+export const getNotification = async (): Promise<
+  INotificationResponseAPI[]
+> => {
+  const res = await axiosInstance.get<INotificationResponseAPI[]>(
+    `/api/v1/notifications`,
+  );
+  if (res.status === 200) {
+    return res.data;
+  }
   return [];
 };
